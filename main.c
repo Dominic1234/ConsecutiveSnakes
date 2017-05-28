465b0a57902f93815e82832a47ce07a9e7ab76b8
Deepak <sharmadpk@gmail.com>
2017-05-28 21:03:09 +0530

    n/2 correction

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ int main()
 		}
 		change = 1;
 		while(change){
-			for(b = 0; b < n; b++)
+			for(b = 1; b < n; b++)
 			{
 				change = 0;
 				if(sx[b] < sx[b-1])
@@ -57,28 +57,29 @@ int main()
 			mid = (sx[n/2-1]+sx[(n+2)/2-1])/2;
 		}
 		if (debug) printf ("mid = %d\n", mid);
-		if(A > mid-(l*n/2))
+		if(A > mid-(l*(n/2)))
 		{
 			start = A;
 			if (debug) printf ("Adjusting start due to left overlap. New = %d\n", start);
 		}
-		else if(B < mid+(l*(n+1)/2))
+		else if(B < mid+((n+1)/2*l))
 		{
-			start = mid-(l*n/2) - (mid+(l*(n+1)/2) - B);
+			start = mid-(n/2*l) - (mid+((n+1)/2*l) - B);
 			if (debug) printf ("Adjusting start due to right overlap. New = %d\n", start);
 		}
 		else
 		{
-			start = mid-(l*n/2);
+			start = mid-(n/2*l);
 		}
 		sp = start;
 		if (debug) printf ("start = %d\n", start);
-		for(int b = 0; b < n; b++)
+		for(b = 0; b < n; b++)
 		 {
 		 sum  = sum + abs(sx[b]-sp);
 		 if (debug) printf ("snake at %d moves %d to %d\n", sx[b], abs(sx[b]-sp), sp);
 		 	sp += l;
 		}
+		if (debug) printf ("=== Answer = ");
 		printf("%d\n", sum);
 			free(sx);
 			free(sy);
